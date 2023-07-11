5a46b8bf67eacb54625c7af36ddcaf484048a95d
regg00 <regg00@gmail.com>
2023-07-10 20:14:54 -0400

    regrouped commong request parameters into a single function

--- PSPsat.psm1
+++ PSPsat.psm1
@@ -52,7 +52,6 @@ function Save-Token {
     }
 }
 
-
 function Send-ApiRequest {
     <#
     .SYNOPSIS
@@ -96,18 +95,30 @@ function Send-ApiRequest {
     Return (Invoke-RestMethod @Arguments)                
 }
 
-function Get-PsatUsers {
+function Set-RequestCommonParameters {
+    <#
+    .SYNOPSIS
+    Helper function to simplify the building of the request parameters hashtable
+
+    #>
     param (
-        [Int32] $PageNumber,    
-        [Int32] $PageSize = 20,
-        [String[]] $UserEmailAddress,
-        [Switch] $IncludeDeletedUsers,                
-        [Switch] $FormatJson
+        [Int32] $PageSize = 20,    
+        [Int32] $PageNumber,            
+        [String[]] $UserEmailAddress,        
+        [String[]] $CampaignName,        
+        [String[]] $Action,
+        [String] $EventTimestampStart,
+        [String] $EventTimestampEnd,
+        [String] $CampaignStartDateStart,
+        [String] $CampaignStartDateEnd,        
+        [String] $ReportedDateStart,
+        [String] $ReportedDateEnd,
+        [String] $ReceivedDateStart,
+        [String] $ReceivedDateEnd
     )
 
-    
     $RequestParameters = @{}
-
+    
     if ($PageSize) {
         $RequestParameters.Add("page[size]", $PageSize)
     }
@@ -118,20 +129,78 @@ function Get-PsatUsers {
 
     if ($UserEmailAddress) {
         $FormattedString = $UserEmailAddress -join ","
-        $RequestParameters.Add("filter[_useremailaddress]", "[$FormattedString]")
-        $RequestParameters
+        $RequestParameters.Add("filter[_useremailaddress]", "[$FormattedString]")                
+    }    
+
+    if ($CampaignName) {
+        $FormattedString = $CampaignName -join ","
+        $RequestParameters.Add("filter[_campaignname]", "[$FormattedString]")
+    }
+
+    if ($Action) {
+        $FormattedString = $Action -join ","
+        $RequestParameters.Add("filter[_action]", "[$FormattedString]")
         
+    }   
+
+    if ($EventTimestampStart) {
+        $RequestParameters.Add("filter[_eventtimestamp_start]", $EventTimestampStart)
     }
 
+    if ($EventTimestampEnd) {
+        $RequestParameters.Add("filter[_eventtimestamp_end]", $EventTimestampEnd)
+    }
+
+    if ($CampaignStartDateStart) {
+        $RequestParameters.Add("filter[_campaignstartdate_start]", $CampaignStartDateStart)
+    }
+
+    if ($CampaignStartDateEnd) {
+        $RequestParameters.Add("filter[_campaignstartdate_end]", $CampaignStartDateEnd)
+    }
+
+    if ($ReportedDateStart) {
+        $RequestParameters.Add("filter[_reporteddate_start]", $ReportedDateStart)
+    }
+
+    if ($ReportedDateEnd) {
+        $RequestParameters.Add("filter[_reporteddate_end]", $ReportedDateEnd)
+    }
+
+    if ($ReceivedDateStart) {
+        $RequestParameters.Add("filter[_receiveddate_start]", $ReceivedDateStart)
+    }
+
+    if ($ReceivedDateEnd) {
+        $RequestParameters.Add("filter[_receiveddate_end]", $ReceivedDateEnd)
+    }
+        
+    Return $RequestParameters
+    
+}
+
+function Get-PsatUsers {
+    param (
+        [Int32] $PageNumber,    
+        [Int32] $PageSize,
+        [String[]] $UserEmailAddress,
+        [Switch] $IncludeDeletedUsers,                
+        [Switch] $FormatJson
+    )
+    
+    $Params = Set-RequestCommonParameters -PageSize $PageSize `
+        -PageNumber $PageNumber `
+        -UserEmailAddress $UserEmailAddress        
+    
     if ($IncludeDeletedUsers) {
-        $RequestParameters.Add("filter[_includedeletedusers]", "TRUE")
+        $Params.Add("filter[_includedeletedusers]", "TRUE")        
     }
-
+    
     if ($FormatJson) {
-        Return (Send-ApiRequest -Endpoint '/users' -Body $RequestParameters) | ConvertTo-Json -Depth 5
+        Return (Send-ApiRequest -Endpoint '/users' -Body $Params) | ConvertTo-Json -Depth 5
     }
     else {
-        $Data = (Send-ApiRequest -Endpoint '/users' -Body $RequestParameters).Data
+        $Data = (Send-ApiRequest -Endpoint '/users' -Body $Params).Data
         $FormattedData = $Data | Select-Object -Property id, type -ExpandProperty attributes
         Return $FormattedData
     }                
@@ -140,44 +209,45 @@ function Get-PsatUsers {
 function Get-PsatPhishing {
     param (
         [Int32] $PageNumber,    
-        [Int32] $PageSize = 20,
+        [Int32] $PageSize,
         [String[]] $UserEmailAddress,
         [String[]] $CampaignName,
         [Switch] $IncludeDeletedUsers,             
-        [Switch] $FormatJson
-        # TODO: Add all parameters
+        [Switch] $FormatJson,
+        [String] $EventTimestampStart,
+        [String] $EventTimestampEnd,
+        [String] $CampaignStartDateStart,
+        [String] $CampaignStartDateEnd,
+        [Switch] $IncludeNoAction,
+        [Switch] $IncludeArchivedCampaigns        
     )
     
-    $RequestParameters = @{}
+    $Params = Set-RequestCommonParameters -PageSize $PageSize `
+        -PageNumber $PageNumber `
+        -UserEmailAddress $UserEmailAddress `
+        -CampaignName $CampaignName `
+        -EventTimestampStart $EventTimestampStart `
+        -EventTimestampEnd $EventTimestampEnd `
+        -CampaignStartDateStart $CampaignStartDateStart `
+        -CampaignStartDateEnd $CampaignStartDateEnd        
 
-    if ($PageSize) {
-        $RequestParameters.Add("page[size]", $PageSize)
+    if ($IncludeNoAction) {
+        $RequestParameters.Add("filter[_includenoaction]", "TRUE")
     }
-
-    if ($PageNumber) {
-        $RequestParameters.Add("page[number]", $PageNumber)
-    }
-
-    if ($UserEmailAddress) {
-        $FormattedString = $UserEmailAddress -join ","
-        $RequestParameters.Add("filter[_useremailaddress]", "[$FormattedString]")
-        
-    }
-
-    if ($CampaignName) {
-        $FormattedString = $CampaignName -join ","
-        $RequestParameters.Add("filter[_campaignname]", "[$FormattedString]")
+    
+    if ($IncludeArchivedCampaigns) {
+        $RequestParameters.Add("filter[_includearchivedcampaigns]", "TRUE")
     }
 
     if ($IncludeDeletedUsers) {
-        $RequestParameters.Add("filter[_includedeletedusers]", "TRUE")
+        $Params.Add("filter[_includedeletedusers]", "TRUE")        
     }
 
     if ($FormatJson) {
-        Return (Send-ApiRequest -ApiVersion '0.3.0' -Endpoint '/phishing' -Body $RequestParameters) | ConvertTo-Json -Depth 5
+        Return (Send-ApiRequest -ApiVersion '0.3.0' -Endpoint '/phishing' -Body $Params) | ConvertTo-Json -Depth 5
     }
     else {
-        $Data = (Send-ApiRequest -ApiVersion '0.3.0' -Endpoint '/phishing' -Body $RequestParameters).Data
+        $Data = (Send-ApiRequest -ApiVersion '0.3.0' -Endpoint '/phishing' -Body $Params).Data
         $FormattedData = $Data | Select-Object -Property id, type -ExpandProperty attributes
         Return $FormattedData
     }                
@@ -186,47 +256,56 @@ function Get-PsatPhishing {
 function Get-PsatPhishAlarm {
     param (
         [Int32] $PageNumber,    
-        [Int32] $PageSize = 20,
+        [Int32] $PageSize,
         [String[]] $UserEmailAddress,
         [ValidateSet("Opened", "Opened in Preview Pane", "Unopened")]        
         [String[]] $Action,   
         [Switch] $IncludeDeletedUsers,             
-        [Switch] $FormatJson
-        # TODO: Add all parameters
+        [Switch] $FormatJson,
+        [String] $ReportedDateStart,
+        [String] $ReportedDateEnd,
+        [String] $ReceivedDateStart,
+        [String] $ReceivedDateEnd,
+        [Switch] $IncludePlatformNotification
+        
     )
     
-    $RequestParameters = @{}
-
-    if ($PageSize) {
-        $RequestParameters.Add("page[size]", $PageSize)
-    }
-
-    if ($PageNumber) {
-        $RequestParameters.Add("page[number]", $PageNumber)
-    }
-
-    if ($UserEmailAddress) {
-        $FormattedString = $UserEmailAddress -join ","
-        $RequestParameters.Add("filter[_useremailaddress]", "[$FormattedString]")
-        
-    } 
+    $Params = Set-RequestCommonParameters -PageSize $PageSize `
+        -PageNumber $PageNumber `
+        -UserEmailAddress $UserEmailAddress `
+        -CampaignName $CampaignName `
+        -ReportedDateStart $ReportedDateStart `
+        -ReportedDateEnd $ReportedDateEnd `
+        -ReceivedDateStart $ReceivedDateStart `
+        -ReceivedDateEnd $ReceivedDateEnd
     
-    if ($Action) {
-        $FormattedString = $Action -join ","
-        $RequestParameters.Add("filter[_action]", "[$FormattedString]")
-        
-    }   
+
+    if ($IncludeNoAction) {
+        $RequestParameters.Add("filter[_includenoaction]", "TRUE")
+    }
+    
+    if ($IncludePlatformNotification) {
+        $RequestParameters.Add("filter[_includeplatformnotifications]", "TRUE")
+    }
 
     if ($IncludeDeletedUsers) {
-        $RequestParameters.Add("filter[_includedeletedusers]", "TRUE")
+        $Params.Add("filter[_includedeletedusers]", "TRUE")        
     }
 
     if ($FormatJson) {
-        Return (Send-ApiRequest -Endpoint '/phishalarm' -Body $RequestParameters) | ConvertTo-Json -Depth 5
+        Return (Send-ApiRequest -Endpoint '/phishalarm' -Body $Params) | ConvertTo-Json -Depth 5
     }
     else {
-        $Data = (Send-ApiRequest -Endpoint '/phishalarm' -Body $RequestParameters).Data
+        $Data = (Send-ApiRequest -Endpoint '/phishalarm' -Body $Params).Data
         $FormattedData = $Data | Select-Object -Property id, type -ExpandProperty attributes
         Return $FormattedData
     }                
 }
+
+
+<#
+TODO:
+    - Knowledge Assessment
+    - Training
+    - Training Enrollments
+#>
